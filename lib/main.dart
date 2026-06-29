bd83d871c7b87661da52d234299517ba4303afb3
Anderson Boeing <anderboeing@gmail.com>
2026-06-29 17:59:01 -0300

    ajsutes layout

--- lib/main.dart
+++ lib/main.dart
@@ -28,54 +28,62 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode entrar!',
-            style: TextStyle(
-              fontSize: 26,
-              color: Colors.white,
-              fontWeight: FontWeight(700),
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/sorvetes.jpg'),
+            fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Pode entrar!',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black,
+                fontWeight: FontWeight(700),
+              ),
+            ),
 
-          Padding(
-            padding: const EdgeInsets.all(32),
-            child: Text(
-              '0',
-              style: TextStyle(fontSize: 100, color: Colors.white),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Text(
+                '0',
+                style: TextStyle(fontSize: 100, color: Colors.black),
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  padding: const EdgeInsets.all(36),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.all(36),
+                  ),
+                  child: const Text(
+                    'saiu',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
                 ),
-                child: const Text(
-                  'saiu',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
+                const SizedBox(width: 32),
+                TextButton(
+                  onPressed: increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    fixedSize: const Size(100, 100),
+                  ),
+                  child: const Text(
+                    'entrou',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
                 ),
-              ),
-              const SizedBox(width: 32),
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                ),
-                child: const Text(
-                  'entrou',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
